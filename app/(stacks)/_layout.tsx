17a39176e994d130ad76ed1473863d5e139d9578
nikita <nikitostimoshenko@gmail.com>
2024-11-19 01:56:16 +0300

    init project

---
+++ app/(stacks)/_layout.tsx
@@ -0,0 +1,28 @@
+import { Stack } from 'expo-router'
+import React from 'react'
+import { Platform } from 'react-native'
+
+export default function StackLayout() {
+	return (
+		<Stack
+			screenOptions={{
+				headerTitleStyle: {
+					fontWeight: 'bold',
+				},
+				gestureEnabled: true,
+				animation: Platform.select({
+					ios: 'default',
+					android: 'slide_from_right',
+				}),
+			}}
+		>
+			<Stack.Screen
+				name='index'
+				options={{
+					title: 'Home',
+				}}
+			/>
+			<Stack.Screen name='create-task' options={{}} />
+		</Stack>
+	)
+}
